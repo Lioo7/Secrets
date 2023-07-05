8e7c0af2627f8845de7e744e9c8424e7aeb0d885
Lioz Akirav <lioz.akirav@gmail.com>
2023-07-05 16:38:33 +0300

    add env vars

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 // eslint-disable-next-line no-unused-vars
@@ -45,8 +47,7 @@ const userSchema = new mongoose.Schema ({
     password: String,
 });
 
-const secret = 'Thisisoutlittlesecret.';
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model('User', userSchema);
 
