b44d928c3a70aca4413ef399af971a47c8aef36d
Lioz Akirav <lioz.akirav@gmail.com>
2023-07-09 22:42:40 +0300

    implement submit + refactor

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Dependencies
+// Set up logger
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
@@ -26,10 +26,12 @@ const logger = winston.createLogger({
 });
 
 if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}
+    logger.add(
+      new winston.transports.Console({
+        format: winston.format.simple()
+      })
+    );
+  }
 
 // Middleware
 app.set('view engine', 'ejs');
@@ -46,18 +48,23 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Database Connection
-mongoose.connect('mongodb://127.0.0.1:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/userDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => {
     logger.info('Connected to MongoDB');
   })
-  .catch((error) => {
+  .catch(error => {
     logger.error('Failed to connect to MongoDB:', error);
   });
 
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -67,20 +74,17 @@ const User = new mongoose.model('User', userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(function(user, cb) {
-    process.nextTick(function() {
-      return cb(null, {
-        id: user.id,
-        username: user.username,
-        picture: user.picture
-      });
-    });
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
   });
   
-  passport.deserializeUser(function(user, cb) {
-    process.nextTick(function() {
-      return cb(null, user);
-    });
+  passport.deserializeUser(async function(id, done) {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
   });
 
 passport.use(new GoogleStrategy({
@@ -120,6 +124,13 @@ app.get('/register', function (req, res) {
 app.post('/register', function (req, res) {
     const {username, password} = req.body;
 
+    // Input validation
+    if (!username || !password) {
+        logger.error('Username or password is missing');
+        res.redirect('/register');
+        return;
+    }
+
     User.register({username: username, active: false}, password, function(err, user) {
         if (err) {
             logger.error('Error registering user:', err);
@@ -139,6 +150,14 @@ app.get('/login', function (req, res) {
 
 app.post('/login', async function (req, res) {
     const {username, password} = req.body;
+
+    // Input validation
+    if (!username || !password) {
+        logger.error('Username or password is missing');
+        res.redirect('/login');
+        return;
+    }
+
     const user = new User({
         username: username,
         password: password
@@ -166,15 +185,55 @@ app.post('/logout', function(req, res){
     });
   });
 
-app.get('/secrets', function(req, res){
-    if (req.isAuthenticated()) {
-        logger.info('The user is authenticated')
-        res.render('secrets');
+app.get('/secrets', async function (req, res) {
+    try {
+        const usersWithSecrets = await User.find({ 'secret': { $ne: null } });
+        if (!usersWithSecrets || usersWithSecrets.length === 0) {
+            logger.error('No secrets found');
+            res.render('secrets', { usersWithSecrets: [] });
+            return;
+        }
+        logger.info('Found secrets');
+        res.render('secrets', { usersWithSecrets: usersWithSecrets });
+    } catch (error) {
+        logger.error('Error fetching secrets', error);
+        res.render('error', { message: 'Error fetching secrets' });
+    }
+});
+
+app.get('/submit', function(req, res){
+    if (req.isAuthenticated()){
+        res.render('submit');
     } else {
-        logger.error('The user is not authenticated');
         res.redirect('/login');
     }
 });
+
+app.post('/submit', async function (req, res) {
+    const secret = req.body.secret;
+
+    // Input validation
+    if (!secret) {
+        logger.error('Secret is missing');
+        return;
+        }
+
+    logger.info('User ID: ' + req.user.id);
+    try {
+      const user = await User.findById(req.user.id);
+      if (!user) {
+        logger.error('User not found');
+      } else {
+        user.secret = secret;
+        await user.save();
+        logger.info('User updated successfully');
+        res.redirect('/secrets');
+      }
+    } catch (error) {
+      logger.error('Error updating user', error);
+      res.render('error', { message: 'Error updating user' });
+    }
+  });
 
 
 // Start the server
